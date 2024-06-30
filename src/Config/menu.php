1bfff4e9b198eaac5ad8f88b70ceb67003225eb0
Sameer Kumar Jain <dev@saachitech.com>
2024-06-30 18:45:39 +0530

    Update menu.php

--- src/Config/menu.php
+++ src/Config/menu.php
@@ -6,5 +6,6 @@ return [
         'name'  => 'bagisto_graphql::app.admin.menu.push-notification',
         'route' => 'admin.settings.push_notification.index',
         'sort'  => 9,
+        'info'  => 'bagisto_graphql::app.admin.configuration.index.general.graphql-api.info'
     ],
 ];
